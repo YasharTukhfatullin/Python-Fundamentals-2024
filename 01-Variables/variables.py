8ca686ea6d6d4ec0932c1b3f46f74559efbc0d1e
AURORA_NT\yashar.tukhfatullin <yashar.tukhfatullin@rovisys.com>
2024-02-21 17:04:50 -0500

    Created the new folder for python and added variable file

---
+++ 01-Variables/variables.py
@@ -0,0 +1,8 @@
+""" 
+Variable = a container for a value. Behaves as the value that it contains.
+"""
+first_name = "Yashar"
+last_name = "Tukhfatullin"
+print(first_name)
+print("Hello " + first_name)
+print(type(first_name))
