daa779bd9cfcc269cf8664722f612102450d2a4b
AURORA_NT\yashar.tukhfatullin <yashar.tukhfatullin@rovisys.com>
2024-02-22 14:46:16 -0500

    How to use math functions

---
+++ 05-MathFunctions/mathFunctions.py
@@ -0,0 +1,16 @@
+import math 
+
+pi = 3.1415
+
+x = 1
+y = 2
+x = 3
+
+print(round(pi))
+print(math.ceil(pi)) # round up
+print(math.floor(pi)) # round down
+print(abs(pi)) # makes negative number to positive
+print(pow(pi, 2))
+print(math.sqrt(pi))
+print(max(x, y, x))
+print(min(x, y, x))
