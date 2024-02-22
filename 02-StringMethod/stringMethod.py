441a263079f6fecb422f96ee799ccce3f2a3727d
AURORA_NT\yashar.tukhfatullin <yashar.tukhfatullin@rovisys.com>
2024-02-22 14:17:23 -0500

    How to use string methods

---
+++ 02-StringMethod/stringMethod.py
@@ -0,0 +1,13 @@
+name = "Yashar"
+
+print(name)
+print(len(name))
+print(name.find("a"))
+print(name.capitalize()) # Only capitalize first letter in the name
+print(name.upper())
+print(name.lower())
+print(name.isdigit())
+print(name.isalpha())
+print(name.count("a"))
+print(name.replace("a", "A"))
+print(name * 3)
