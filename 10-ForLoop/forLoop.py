a2e78eeafc82986129a982522c68610a7fd5f252
AURORA_NT\yashar.tukhfatullin <yashar.tukhfatullin@rovisys.com>
2024-02-23 10:35:26 -0500

    How to use for loop

---
+++ 10-ForLoop/forLoop.py
@@ -0,0 +1,25 @@
+"""
+for loop = a statement that will execute it's block of code a limited amount of times.
+
+while loop = unlimited times
+
+for loop = limited times
+"""
+import time
+
+for i in range(10):
+  print(i)
+print("===========")
+
+for i in range(50, 100):
+  print(i)
+print("===========")
+
+for i in "Yashar Tukhfatullin":
+  print(i)
+print("===========")
+
+for seconds in range(10, 0, -1):
+  print(seconds)
+  time.sleep(1)
+print("Happy New Year")
