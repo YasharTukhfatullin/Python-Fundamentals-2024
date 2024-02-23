7a0c25ca88ed3a626d133b692090cb2453837cb0
AURORA_NT\yashar.tukhfatullin <yashar.tukhfatullin@rovisys.com>
2024-02-23 10:17:13 -0500

    How to use while loop

---
+++ 09-WhileLoop/whileLoop.py
@@ -0,0 +1,20 @@
+"""
+while loop = a statement that will execute it's block of code,
+            as long as it's condition remains true.
+"""
+
+name = ""
+
+while len(name) == 0:
+  name = input("Enter your name: ")
+  
+print("Hello " + name)
+print("===============")
+
+
+new_name = None # instead of null python uses None
+
+while not new_name:
+  new_name = input("Enter your new name?")
+
+print("Hi " + new_name)
