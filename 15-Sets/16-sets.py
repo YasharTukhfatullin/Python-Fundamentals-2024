46ffff353f67a03db984921f88a7c924678dc158
AURORA_NT\yashar.tukhfatullin <yashar.tukhfatullin@rovisys.com>
2024-02-23 12:15:17 -0500

    How to use  sets

---
+++ 15-Sets/16-sets.py
@@ -0,0 +1,25 @@
+"""
+sets = collection which is unordered, unindexed. No duplicate values.
+"""
+
+utensils = {"fork", "spoon", "knife", "knife", "knife"}
+
+for x in utensils:
+  print(x)
+
+utensils.add("napkin")
+print(utensils)
+
+utensils.remove("knife")
+print(utensils)
+print("==================")
+
+dishes = {"bowl", "plate", "cup", "knife"}
+
+utensils.update(dishes)
+print(utensils)
+
+dinner_table = utensils.union(dishes)
+print(dinner_table)
+
+print(dishes.difference(utensils))
