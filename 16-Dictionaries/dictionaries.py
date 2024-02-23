691b0ae4b7f44f693a94e7b36a2e514e26857a8c
AURORA_NT\yashar.tukhfatullin <yashar.tukhfatullin@rovisys.com>
2024-02-23 12:30:49 -0500

    How to use dictionary

---
+++ 16-Dictionaries/dictionaries.py
@@ -0,0 +1,25 @@
+"""
+dictionary = a changeable, unordered collection of unique key:value pairs
+            Fast because they use hashing, allow us to access a value quickly.
+"""
+
+capitals = {"USA": "Washington DC",
+            "India": "New Delhi",
+            "China": "Beijing",
+            "Russia": "Moscow"}
+
+print(capitals)
+print(capitals.get("Russia"))
+print(capitals.get("Germany")) # use get method 
+print(capitals.keys())
+print(capitals.values())
+print(capitals.items())
+
+for key, value in capitals.items():
+  print(key, value)
+
+capitals.update({"Germany": "Berlin"})
+print(capitals)
+
+capitals.pop("China")
+print(capitals)
