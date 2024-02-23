042b49dd93c3caf470cf393989e7875541cef597
AURORA_NT\yashar.tukhfatullin <yashar.tukhfatullin@rovisys.com>
2024-02-23 11:41:14 -0500

    How to use tuples

---
+++ 14-tuples/tuples.py
@@ -0,0 +1,18 @@
+"""
+tuples = collection which is order and unchangeable
+          used to group together related data.
+"""
+
+student = ("Kyle", 21, "male")
+print(student)
+
+print(student.count("Kyle"))
+print(student.index("male"))
+print("===============")
+
+for x in student:
+  print(x)
+print("===============")
+
+if "Kyle" in student:
+  print("Kyle is here")
