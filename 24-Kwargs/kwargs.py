b86ae46551e9585bd525f7adf4cd07a43ef0a9ce
AURORA_NT\yashar.tukhfatullin <yashar.tukhfatullin@rovisys.com>
2024-02-23 14:57:22 -0500

    How to use kwargs

---
+++ 24-Kwargs/kwargs.py
@@ -0,0 +1,12 @@
+"""
+**kwargs = parameters that will pack all arguments into a dictionary
+          useful so that a function can accept a varying amount of keywords arguments.
+"""
+def hello(**kwargs):
+  # print("Hello " + kwargs["first"] + kwargs["last"])
+  print("Hello", end=" ")
+
+  for key, value in kwargs.items():
+    print(value, end=" ")
+
+hello(title="Mr.", first="Bro", middle="Dude", last="Code")
