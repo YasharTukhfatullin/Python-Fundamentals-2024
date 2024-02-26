fcd497085c5c8b057fde685699ad25543b763725
AURORA_NT\yashar.tukhfatullin <yashar.tukhfatullin@rovisys.com>
2024-02-26 11:17:30 -0500

    How to detect file or folder

---
+++ 28-FileDetection/fileDetection.py
@@ -0,0 +1,12 @@
+import os 
+
+path = "C:\\Users\\yashar.tukhfatullin\\Desktop\\Computer Science Tutorial\\Python-Fundamentals-2024\\28-FileDetection\\variables.txt"
+
+if os.path.exists(path):
+  print("This location is exists")
+  if os.path.isfile(path):
+    print("This is a file variable")
+  elif os.path.isdir(path): # checking if folder is exits
+    print("That is directory (folder)")
+else:
+  print("This location doesn't exists")
