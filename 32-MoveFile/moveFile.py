a5dc148892b423df43115daecc787ade23dae572
AURORA_NT\yashar.tukhfatullin <yashar.tukhfatullin@rovisys.com>
2024-02-26 15:02:23 -0500

    How to delete a file

---
+++ 32-MoveFile/moveFile.py
@@ -0,0 +1,12 @@
+import os 
+
+source = "C:\\Users\\yashar.tukhfatullin\\Desktop\\Computer Science Tutorial\\test.txt"
+destination = "C:\\Users\\yashar.tukhfatullin\\Desktop\\Computer Science Tutorial\\Python-Fundamentals-2024\\32-MoveFile\\test.txt"
+
+try:
+  if os.path.exists(destination):
+    print("There is already a file there")
+  else:
+    os.replace(source, destination)
+except FileNotFoundError:
+  print(f"{source} was not found  ")
---
+++ 33-DeleteFile/deleteFile.py
@@ -0,0 +1,17 @@
+import os 
+
+path="C:\\Users\\yashar.tukhfatullin\\Desktop\\Computer Science Tutorial\\Python-Fundamentals-2024\\33-DeleteFile\\test.txt"
+
+try:
+  # os.remove(path)     delete a file
+  # os.rmdir(path)      delete an empty directory
+  # shutil.rmtree(path) delete a directory containing files
+  os.remove(path)
+except FileNotFoundError:
+  print("That file was not found")
+except PermissionError:
+  print("You got not have permission to delete that")
+except OSError:
+  print("You dont not have permission to delete that")
+else:
+  print(f"{path} was deleted ")
