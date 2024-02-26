01617118732a8a8459435ea68f2bce7c1b26c031
AURORA_NT\yashar.tukhfatullin <yashar.tukhfatullin@rovisys.com>
2024-02-26 10:30:19 -0500

    How to use string formatting.

---
+++ 25-StringFormating/stringFormating.py
@@ -0,0 +1,18 @@
+"""
+str.format() = optional method that gives users
+              more control when displaying output
+"""
+animal = "cow"
+item = "moon"
+
+print("The " + animal + " jumped over the " + item)
+
+print("The {} jumped over the {}".format("cow", "moon"))
+print("The {} jumped over the {}".format(animal, item))
+print("The {0} jumped over the {1}".format(animal, item))
+print("The {animal} jumped over the {item}".format(animal="cow", item="moon"))
+print(f"The {animal} jumped over the {item}")
+print(f"==================")
+
+number = 3.141592
+print(f"The number pi is {number}")
