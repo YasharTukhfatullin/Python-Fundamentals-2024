33fc090b0641104f92fd4265c8093864a0190fac
AURORA_NT\yashar.tukhfatullin <yashar.tukhfatullin@rovisys.com>
2024-02-26 11:48:26 -0500

    How to read a file

---
+++ 29-ReadFile/readFile.py
@@ -0,0 +1,5 @@
+try:
+  with open("C:\\Users\\yashar.tukhfatullin\\Desktop\\Computer Science Tutorial\\Python-Fundamentals-2024\\29-ReadFile\\test.txt") as file:
+    print(file.read())
+except FileNotFoundError:
+  print("That file was not file")
