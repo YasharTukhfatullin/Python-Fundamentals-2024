ebd3535ced2775f05343f68d85c46982397a3301
AURORA_NT\yashar.tukhfatullin <yashar.tukhfatullin@rovisys.com>
2024-02-26 11:55:43 -0500

    How to write in a file and append.

---
+++ 30-WriteFile/writeFile.py
@@ -0,0 +1,9 @@
+text = "Hello my name is David.\nThis is some new text\nHave a nice day.\n"
+
+with open("C:\\Users\yashar.tukhfatullin\\Desktop\\Computer Science Tutorial\\Python-Fundamentals-2024\\30-WriteFile\\test.txt", "w") as file:
+  file.write(text)
+
+# How to append a file 
+
+# new_text = "This is how you append a file"
+# with open("C:\\Users\yashar.tukhfatullin\\Desktop\\Computer Science Tutorial\\Python-Fundamentals-2024\\30-WriteFile\\test.txt", # "a") as file:
