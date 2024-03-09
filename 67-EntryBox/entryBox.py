87b47d4dc89928067d66cfa147b03039ccc3b54c
AURORA_NT\yashar.tukhfatullin <yashar.tukhfatullin@rovisys.com>
2024-03-09 18:23:47 -0500

    How to use entry box

---
+++ 67-EntryBox/entryBox.py
@@ -0,0 +1,41 @@
+from tkinter import *
+
+
+def submit():
+    username = entry.get()
+    print(f"Hello {username}")
+
+
+def delete():
+    entry.delete(0, END)
+
+
+def backspace():
+    entry.delete(len(entry.get()) - 1, END)
+
+
+window = Tk()
+
+entry = Entry(
+    window,
+    font=("Arial", 30),
+    fg="#00FF00",
+    bg="black",  # frontground = font color
+    show="*",
+)
+
+entry.insert(0, "Ryan")
+
+entry.pack(side=LEFT)
+
+
+submit_button = Button(window, text="submit", command=submit)
+submit_button.pack(side=RIGHT)
+
+delete_button = Button(window, text="delete", command=delete)
+delete_button.pack(side=RIGHT)
+
+backspace_button = Button(window, text="backspace", command=backspace)
+backspace_button.pack(side=RIGHT)
+
+window.mainloop()
